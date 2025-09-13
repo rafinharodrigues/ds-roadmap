323c36b1404f52b8263808b660ccb153dcabb75e
Rafael Rodrigues <elliot@alu.ufc.br>
2025-09-13 19:52:35 -0300

    feat(12): add swimmable class

---
+++ months/2025/1_september/day_14/12_swimable_class.py
@@ -0,0 +1,20 @@
+# Exercise: 12. Criar classe Swimmable com método swim()
+
+class Flyable():
+    def __init__(self) -> None:
+        """Initiate a flyable object."""
+        pass
+
+    def fly(self) -> None:
+        """Start's flying."""
+        print('Flying.')
+
+
+class Swimmable():
+    def __init__(self) -> None:
+        """Inititante a swimmable object."""
+        pass
+
+    def swim(self) -> None:
+        """Start's swimming."""
+        print('Swimming.')
