d28bae45ff0c199e0b540025d706a430e4282f2b
Rafael Rodrigues <elliot@alu.ufc.br>
2025-09-13 11:09:33 -0300

    feat(9): add circle class with private radius attibute

---
+++ months/2025/1_september/day_12/9_circle_class.py
@@ -0,0 +1,6 @@
+# Exercise: 9. Implementar classe Circle com raio privado
+
+class Circle:
+    def __init__(self, radius: int) -> None:
+        """Initiate a circle object."""
+        self.__radius = radius
