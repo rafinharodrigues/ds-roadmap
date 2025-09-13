d417c5d4760e295cdd66f65fecbbda4c0cf73945
Rafael Rodrigues <elliot@alu.ufc.br>
2025-09-13 10:55:17 -0300

    feat(6): add class rectangle

---
+++ months/2025/1_september/day_12/6_class_rectangle.py
@@ -0,0 +1,7 @@
+# Exercise: 6. Implementar classe Rectangle com _width, _height (private
+
+class Rectangle:
+    def __init__(self, width: int, height: int) -> None:
+        """Initiante a rectangle object."""
+        self._width = width
+        self._height = height
