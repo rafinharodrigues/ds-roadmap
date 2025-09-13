244f6a4d7d1dee350e0a7a396c64b6b4fe70ba26
Rafael Rodrigues <elliot@alu.ufc.br>
2025-09-13 19:52:20 -0300

    feat(11): add flyable class

---
+++ months/2025/1_september/day_14/11_flyable_class.py
@@ -0,0 +1,10 @@
+# Exercise: 11. Criar classe Flyable com método fly()
+
+class Flyable():
+    def __init__(self) -> None:
+        """Initiate a flyable object."""
+        pass
+
+    def fly(self) -> None:
+        """Start's flying."""
+        print('Flying.')
